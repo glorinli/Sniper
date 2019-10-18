9caa013601bc690f6b97a8a068f7932f381eca10
liguorong <liguorong@xiaoyouzi.com>
2019-10-18 16:15:59 +0800

    Modify project structure.

--- sniper-plugin/src/main/groovy/com/glorinli/sniper/plugin/SniperPlugin.groovy
+++ sniper-plugin/src/main/groovy/com/glorinli/sniper/plugin/SniperPlugin.groovy
@@ -1,6 +1,6 @@
 package com.glorinli.sniper.plugin
 
-import com.android.build.gradle.BaseExtension
+import com.android.build.gradle.AppExtension
 import com.glorinli.sniper.internal.SniperTransform
 import org.gradle.api.Plugin
 import org.gradle.api.Project
@@ -9,7 +9,13 @@ class SniperPlugin implements Plugin<Project> {
 
     @Override
     void apply(Project project) {
-        project.getExtensions().findByType(BaseExtension.class)
+        println("===========SniperPlugin begin===============")
+
+        project.dependencies {
+            implementation 'com.glorinli:sniper:0.0.1-SNAPSHOT'
+        }
+
+        project.getExtensions().findByType(AppExtension)
                 .registerTransform(new SniperTransform())
     }
 }
--- sniper-plugin/src/main/java/com/glorinli/sniper/internal/SniperTransform.java
+++ sniper-plugin/src/main/java/com/glorinli/sniper/internal/SniperTransform.java
@@ -1,12 +1,19 @@
 package com.glorinli.sniper.internal;
 
+import com.android.build.api.transform.DirectoryInput;
+import com.android.build.api.transform.JarInput;
 import com.android.build.api.transform.QualifiedContent;
 import com.android.build.api.transform.Transform;
 import com.android.build.api.transform.TransformException;
+import com.android.build.api.transform.TransformInput;
 import com.android.build.api.transform.TransformInvocation;
 import com.android.build.gradle.internal.pipeline.TransformManager;
 
+import org.gradle.internal.impldep.org.apache.commons.codec.digest.DigestUtils;
+
+import java.io.File;
 import java.io.IOException;
+import java.util.Collection;
 import java.util.Set;
 
 public class SniperTransform extends Transform {
@@ -33,5 +40,38 @@ public class SniperTransform extends Transform {
     @Override
     public void transform(TransformInvocation transformInvocation) throws TransformException, InterruptedException, IOException {
         super.transform(transformInvocation);
+
+        Collection<TransformInput> inputs = transformInvocation.getInputs();
+        for (TransformInput input : inputs) {
+            for (JarInput jarInput : input.getJarInputs()) {
+                transformJar(transformInvocation, jarInput);
+            }
+
+            for (DirectoryInput directoryInput : input.getDirectoryInputs()) {
+                transformDirectory(transformInvocation, directoryInput);
+            }
+        }
+    }
+
+    /**
+     * Transform directory classes
+     */
+    private void transformDirectory(TransformInvocation invocation, DirectoryInput input) {
+
+    }
+
+    /**
+     * Transform jar classes
+     */
+    private void transformJar(TransformInvocation invocation, JarInput input) {
+        File tempDir = invocation.getContext().getTemporaryDir();
+
+        String destName = input.getFile().getName();
+        String hexName = DigestUtils.md5Hex(input.getFile().getAbsolutePath().substring(0, 8));
+        if (destName.endsWith(".jar")) {
+            destName = destName.substring(0, destName.length() - 4);
+        }
+
+        // Get output path
     }
 }
